70699fa03cd3bade3831317821a42e96c7029c7b
Noxara17 <aaron.bear.staehr@gmail.com>
2016-05-12 18:56:32 +1000

    Add files via upload

--- player.js
+++ player.js
@@ -1,18 +1,41 @@
+var LEFT = 0;
+var RIGHT = 1;
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_MAX = 6;
 var Player = function() {
-this.image = document.createElement("img");
+this.sprite = new Sprite("ChuckNorris.png");
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[0, 1, 2, 3, 4, 5, 6, 7]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[8, 9, 10, 11, 12]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[52, 53, 54, 55, 56, 57, 58, 59]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[60, 61, 62, 63, 64]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+[65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+for(var i=0; i<ANIM_MAX; i++)
+{
+this.sprite.setAnimationOffset(i, -55, -87);
+}
 this.position = new Vector2();
 this.position.set(1*TILE, 10*TILE);
 this.width = 159;
-this.height = 163;
-this.offset = new Vector2();
-this.offset.set(-55, -87);
+this.height = 163
 this.velocity = new Vector2();
 this.falling = true;
 this.jumping = false;
-this.image.src = "hero.png";
 };
 Player.prototype.update = function(deltaTime)
 {
+this.sprite.update(deltaTime);
  var left = false;
  var right = false;
  var jump = false;
@@ -111,9 +134,6 @@ this.velocity.x = 0; // stop horizontal velocity
 
 Player.prototype.draw = function()
 {
-context.save();
-context.translate(this.position.x, this.position.y);
-context.rotate(this.rotation);
-context.drawImage(this.image, -this.width/2, -this.height/2);
-context.restore();
+this.sprite.draw(context, this.position.x, this.position.y);
+
 }
